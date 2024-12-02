c52e2267dc9360bc438f5b027b2a340ab63dfc27
Daniyal Hussain <dah001@edu.zealand.dk>
2024-12-02 14:51:23 +0100

    Senstorclass

--- KlasseLib/Sensor.cs
+++ KlasseLib/Sensor.cs
@@ -1,8 +1,24 @@
-﻿namespace KlasseLib
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasseLib
 {
     public class Sensor
     {
+        public int Id { get; }
+        public string SensorType { get; }
+        public double CurrentValue { get; }
+        public DateTime LastMeasurement { get; }
 
-
+        public Sensor(int id, string sensorType, double currentValue, DateTime lastMeasurement)
+        {
+            Id = id;
+            SensorType = sensorType;
+            CurrentValue = currentValue;
+            LastMeasurement = lastMeasurement;
+        }
     }
 }
