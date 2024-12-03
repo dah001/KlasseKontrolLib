f9c70d481144c002061c9880ce96d47a0387c1b6
abdi0736 <151497525+abdi0736@users.noreply.github.com>
2024-12-03 13:53:35 +0100

    ok

--- KlasseTestProject/SensorTest.cs
+++ KlasseTestProject/SensorTest.cs
@@ -17,7 +17,8 @@ namespace KlasseTestProject
             var expectedLastMeasurement = DateTime.Now;
 
             // Act
-            var sensor = new TemperatureSensor(expectedId, expectedSensorType, expectedCurrentValue, expectedLastMeasurement);
+            var sensor = new TemperatureSensor(expectedId, expectedSensorType, expectedCurrentValue,
+                expectedLastMeasurement);
 
             // Assert
             Assert.AreEqual(expectedId, sensor.Id);
@@ -25,13 +26,13 @@ namespace KlasseTestProject
             Assert.AreEqual(expectedCurrentValue, sensor.CurrentValue);
             Assert.AreEqual(expectedLastMeasurement, sensor.LastMeasurement);
         }
-    
+
         // Test method for the Sound sensor
         [TestMethod]
         public void Constructor_ShouldSetSoundValuesCorrectly()
         {
             // Arrange
-            var expectedId = 1; 
+            var expectedId = 1;
             var expectedSensorType = "Sound";
             var expectedCurrentValue = 75.0;
             var expectedLastMeasurement = DateTime.Now;
@@ -46,5 +47,31 @@ namespace KlasseTestProject
             Assert.AreEqual(expectedLastMeasurement, sensor.LastMeasurement);
         }
     }
-    
+
+    [TestClass]
+    public class MotionSensorTest
+    {
+        [TestMethod]
+        public void Constructor_Shouldgetstudentcount()
+        {
+            //Arrange
+            var expectedId = 2;
+            var expectedSensorType = "motion";
+            var expectedCurrentValue = 0.0;
+            var expectedLastMeasurement = DateTime.Now;
+
+            //Act
+            var sensor = new MotionSensor(expectedId, expectedSensorType, expectedCurrentValue,
+                expectedLastMeasurement);
+
+            // Assert
+            Assert.AreEqual(expectedId, sensor.Id, "Id should be correctly set by the constructor.");
+            Assert.AreEqual(expectedSensorType, sensor.SensorType,
+                "SensorType should be correctly set by the constructor.");
+            Assert.AreEqual(expectedCurrentValue, sensor.CurrentValue,
+                "CurrentValue should be correctly set by the constructor.");
+            Assert.AreEqual(expectedLastMeasurement, sensor.LastMeasurement,
+                "LastMeasurement should be correctly set by the constructor.");
+        }
+    }
 }
